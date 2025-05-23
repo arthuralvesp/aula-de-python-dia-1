96060ffaa834c03fee93e73e4ee27b6ef6c8ba84
arthuralvesp <arthurtuiooo@gmail.com>
2025-05-23 16:48:12 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,9 @@
+
+
+nome str(input('qual o seu nome'))
+idade str(input('qual a sua idade'))
+serie str(input('qual a sua serie '))
+
+print ('o seu nome é' ,nome,'a sua idade é',idade,'e sua serie é',serie)
+
+
